20fb035bf6d04581cf569dc54c1913ba24b8e960
Ajith04 <sritharanajith04@gmail.com>
2024-10-11 15:36:46 +0530

    Q2

---
+++ Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+    internal class Course
+    {
+        private string CourseId;
+        private string Title;
+        private string Duration;
+        private decimal Price;
+    }
+}
