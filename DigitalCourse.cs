daed6b2c13b0631459a793c8ac9279155b4c592a
Ajith04 <sritharanajith04@gmail.com>
2024-10-11 16:34:50 +0530

    Q6.a

---
+++ DigitalCourse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+    internal class DigitalCourse : Course
+    {
+
+        public string CourseLink { get; set; }
+        public string FileSize { get; set; }
+        public DigitalCourse(string courseId, string title, string duration, decimal price) : base(courseId, title, duration, price)
+        {
+            
+        }
+    }
+}
---
+++ OnPremise.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+    internal class OnPremise : Course
+    {
+
+        public string Schedule {  get; set; }
+        public string ClassroomCapacity { get; set; }
+        public OnPremise(string courseId, string title, string duration, decimal price) : base(courseId, title, duration, price)
+        {
+        }
+    }
+}
