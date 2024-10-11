ed01183c93309dbd60f78ce36c7c55e3a345cd7b
Ajith04 <sritharanajith04@gmail.com>
2024-10-11 15:44:25 +0530

    Q3.c

--- Program.cs
+++ Program.cs
@@ -10,6 +10,10 @@ namespace CourseManagementSystem_v1
     {
         static void Main(string[] args)
         {
+
+            Course course1 = new Course("C_001", "python for beginners", "3 months", 10);
+            Console.WriteLine(course1);
+            Console.ReadKey();
         }
     }
 }
